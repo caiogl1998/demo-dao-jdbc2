b455b6ebfd6c3c8ec7fa8fd5a3053dfa44372182
Caio <caiogl1998@gmail.com>
2023-12-04 12:43:58 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+		
 		System.out.println(obj);
 		
+		System.out.println(seller);
 		
 	}
 }
