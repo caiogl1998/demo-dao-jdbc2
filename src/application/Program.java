69eb389f43ac8a4dfc19096921931394c0a60324
Caio <caiogl1998@gmail.com>
2023-12-04 15:16:36 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,5 +35,12 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Gred", "greg@gmail.com", LocalDate.now() , 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
